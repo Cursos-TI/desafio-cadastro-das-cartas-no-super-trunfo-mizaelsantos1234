5fdab677b3a70d691e30fd8af442a31d08f3bfc0
mizaelsantos1234 <mizaeelsantos056@gmail.com>
2025-03-07 19:16:55 +0000

    desafio aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,53 +1,15 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste Mizael
-
 int main() {
     char estado, codigoCarta[50], nomeCidade[50];
     int populacao, numeroDePontos;
     float area, pib;
+    float densidadePopulacional, pibPerCapita;
 
+    // CARTA 1
     printf("CARTA 1: \n");
     printf("Digite uma letra de um estado de A á H: \n");
-    scanf("%c", &estado);
-
-    printf("Digite o codigo da carta: \n");
-    scanf("%s", codigoCarta);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", nomeCidade);
-
-    printf("Digite o numero de habitantes: \n");
-    scanf("%d", &populacao);
-
-    printf("Digite o tamanho da area: \n");
-    scanf("%f", &area);
-
-    printf("Digite o PIB: \n");
-    scanf("%f", &pib);
-
-    printf("Digite o numero de pontos: \n");
-    scanf("%d", &numeroDePontos);
-    
-
-    printf("CARTA 1: \n");
-    printf("Estado: %c \n", estado);
-    printf("Codigo: %s \n", codigoCarta);
-    printf("Nome da cidade: %s \n", nomeCidade);
-    printf("População: %d \n", populacao);
-    printf("Área: %.2f \n", area);
-    printf("PIB: %.2f \n", pib);
-    printf("Número de pontos: %d \n", numeroDePontos);
-
-
-    printf("CARTA 2: \n");
-    
-    printf("Digite uma letra de um estado de A á H: \n");
-    scanf("%c", &estado);
+    scanf(" %c", &estado);
 
     printf("Digite o codigo da carta: \n");
     scanf("%s", codigoCarta);
@@ -67,6 +29,49 @@ int main() {
     printf("Digite o numero de pontos: \n");
     scanf("%d", &numeroDePontos);
 
+    //PARA CALCULAR DENSIDADE DA POPULAÇÃO E PIB PER CAPITA:
+    densidadePopulacional = (float)populacao / area;
+    pibPerCapita = pib / populacao;
+
+    printf("CARTA 1: \n");
+    printf("Estado: %c \n", estado);
+    printf("Codigo: %s \n", codigoCarta);
+    printf("Nome da cidade: %s \n", nomeCidade);
+    printf("População: %d \n", populacao);
+    printf("Área: %.2f \n", area);
+    printf("PIB: %.2f \n", pib);
+    printf("Número de pontos: %d \n", numeroDePontos);
+    printf("Densidade Populacional: %.2f \n", densidadePopulacional);
+    printf("PIB per Capita: %.2f \n", pibPerCapita);
+
+    // CARTA 2
+    printf("CARTA 2: \n");
+
+    printf("Digite uma letra de um estado de A á H: \n");
+    scanf(" %c", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", codigoCarta);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", nomeCidade);
+
+    printf("Digite o numero de habitantes: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite o tamanho da area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o numero de pontos: \n");
+    scanf("%d", &numeroDePontos);
+
+    //PARA CALCULAR DENSIDADE DA POPULAÇÃO E PIB PER CAPITA:
+    densidadePopulacional = (float)populacao / area;
+    pibPerCapita = pib / populacao;
+
     printf("CARTA 2: \n");
     printf("Estado: %c \n", estado);
     printf("Codigo: %s \n", codigoCarta);
@@ -75,7 +80,8 @@ int main() {
     printf("Área: %.2f \n", area);
     printf("PIB: %.2f \n", pib);
     printf("Número de pontos: %d \n", numeroDePontos);
-
+    printf("Densidade Populacional: %.2f \n", densidadePopulacional);
+    printf("PIB per Capita: %.2f \n", pibPerCapita);
 
     return 0;
 }
